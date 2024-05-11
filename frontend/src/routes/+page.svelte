0db6ed7ac4289fc16e131b2a13dd10c8baa89e8a
Maximilian Schmidt <maximilian5189@gmail.com>
2024-05-11 08:53:02 -0400

    frontend

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	const getTransactions = async () => {
+		const options = {
+			method: 'GET',
+			// headers: {
+			// 	'Content-Type': 'application/json'
+			// },
+			mode: 'cors'
+		};
+		let res = await fetch('http://localhost:8080/transactions', options);
+		console.log(await res.json());
+	};
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={getTransactions}>klick</button>
