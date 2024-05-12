a6e93dabb9a41e1a0a943578151ae49a4d0dbfae
Maximilian Schmidt <maximilian5189@gmail.com>
2024-05-12 15:23:43 -0400

    getStartOfWeekTimestamp

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -5,3 +5,17 @@ export type transaction = {
 	name: string;
 	amount: number;
 };
+
+export function getStartOfWeekTimestamp(offset = 0) {
+	const today = new Date();
+
+	// day of the week is like so: 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+	const dayOfWeek = today.getDay();
+	const diffToMonday = (dayOfWeek === 0 ? 6 : dayOfWeek - 1) * 24 * 60 * 60 * 1000;
+
+	const startOfWeek = new Date(today.getTime() - diffToMonday);
+	startOfWeek.setDate(startOfWeek.getDate() + offset * 7);
+	startOfWeek.setHours(0, 0, 0, 0);
+
+	return startOfWeek.getTime();
+}
