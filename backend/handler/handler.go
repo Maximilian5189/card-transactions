d673270ae1a073937527402ddb08ad796c1343b2
Maximilian Schmidt <maximilian5189@gmail.com>
2024-05-12 15:23:05 -0400

    url param

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -5,6 +5,7 @@ import (
 	"backend/logger"
 	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -32,14 +33,23 @@ func (handler *Handler) GetTransactions(logger logger.Logger) func(http.Response
 			return
 		}
 
-		// TODO maybe move from/to logic to frontend?
-		now := time.Now()
-		offset := int(time.Monday - now.Weekday())
-		if offset > 0 {
-			offset -= 7 // Adjust for the week starting on Monday
+		var from time.Time
+		if r.URL.Query().Get("from") != "" {
+			f := r.URL.Query().Get("from")
+			fromInt64, err := strconv.ParseInt(f, 10, 64)
+			if handler.handleErr(err, w) != nil {
+				return
+			}
+			from = time.Unix(fromInt64, 0)
+		} else {
+			now := time.Now()
+			offset := int(time.Monday - now.Weekday())
+			if offset > 0 {
+				offset -= 7 // Adjust for the week starting on Monday
+			}
+			from = now.AddDate(0, 0, offset)
+			from = time.Date(from.Year(), from.Month(), from.Day(), 0, 0, 0, 0, from.Location())
 		}
-		from := now.AddDate(0, 0, offset)
-		from = time.Date(from.Year(), from.Month(), from.Day(), 0, 0, 0, 0, from.Location())
 
 		to := from.AddDate(0, 0, 6)
 		to = time.Date(to.Year(), to.Month(), to.Day(), 23, 59, 59, 0, to.Location())
