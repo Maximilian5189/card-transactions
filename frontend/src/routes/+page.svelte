805bdfccb9eb6dc2fbf9716c60d4d8970c2b7758
Maximilian Schmidt <maximilian5189@gmail.com>
2024-05-16 17:21:58 -0400

    totalLastWeek and token frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-	import type { transaction } from '$lib';
+	import { getTransactionsTotal, type transaction, getStartOfWeekTimestamp } from '$lib';
 	import { useSelector, PluginPosition } from 'gridjs';
 	import Grid from 'gridjs-svelte';
 	import 'gridjs/dist/theme/mermaid.css';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+
+	let token: string;
 
 	const columns = [
 		{ name: 'name', sort: false },
@@ -29,7 +33,6 @@
 
 			let total = 0;
 			for (const row of data.toArray()) {
-				console.log(row[1]);
 				total += Number(row[1]);
 			}
 
@@ -38,34 +41,35 @@
 		position: PluginPosition.Footer
 	};
 
-	let total = 0;
 	const getTransactions = (transactions: transaction[]) => {
-		transactions.forEach((transaction: transaction) => {
-			total += Number(transaction.amount);
-		});
-		transactions.sort((a, b) => b.date - a.date);
-		total = Math.round(total * 100) / 100;
 		return transactions.map((transaction) => {
 			return [transaction.name, transaction.amount, transaction.date];
 		});
 	};
+
+	let totalLastWeek = 0;
+	onMount(async () => {
+		token = $page.url.searchParams.get('t') || '';
+
+		const t = getStartOfWeekTimestamp(-1);
+		totalLastWeek = await getTransactionsTotal(t, token);
+	});
 </script>
 
 <h1>Welcome to Jochen</h1>
-
-<div>total: {total}</div>
-
 <Grid
 	{columns}
 	sort
 	search
 	pagination={{ enabled: true, limit: 100 }}
 	server={{
-		url: 'http://localhost:8080/transactions',
+		url: `http://localhost:8080/transactions?t=${token}`,
 		then: getTransactions
 	}}
 	plugins={[sumPlugin]}
 />
+
+<div>total last week: {totalLastWeek}</div>
 
 <style global>
 	@import 'https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css';
